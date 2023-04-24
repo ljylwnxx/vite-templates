6ad4a6616c4263da89a729d74a63db435ab66597
ljylwnxx <ljylwnxx@163.com>
2023-04-24 15:03:52 +0800

    fix

--- vite-vue3-ts-pinia/src/main.ts
+++ vite-vue3-ts-pinia/src/main.ts
@@ -1,15 +1,13 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import store from './store '
-import router from '@/router';
+import store from './store'
+import router from './router'
 
- // 创建vue实例
+// 创建vue实例
 const app = createApp(App)
-// 挂载pinia
-app.use({
-    store,
-    router
-})
+
+app.use(store)
+app.use(router)
+
 // 挂载实例
 app.mount('#app')
